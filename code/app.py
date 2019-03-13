68216d383dd16a8af52babc88c1674aef95e2fa0
Nihal <nihalkaradan@gmail.com>
2019-03-13 06:33:48 +0530

    world is wonderful

--- code/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_restful import Api
-from city import City
-app=Flask(__name__)
-app.secret_key='nihal'
-api = Api(app)
-
-api.add_resource(City,'/city')
-app.run()
