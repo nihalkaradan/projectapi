c6400b6bea18d7d7a4f1be9294d5fc6abaf3d2b1
Nihal <nihalkaradan@gmail.com>
2019-03-13 08:29:09 +0530

    hello world

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import os
+import sqlite3
 from flask import Flask
 from flask_restful import Api
 from city import City
 app=Flask(__name__)
 app.secret_key='nihal'
+DATABASE=os.path.join(os.getcwd(), 'dbs/data.db')
 api = Api(app)
 
 api.add_resource(City,'/city')
--- city.py
+++ city.py
@@ -1,8 +1,10 @@
 from flask_restful import Resource
+import os
 import sqlite3
+DATABASE=os.path.join(os.getcwd(), 'dbs/data.db')
 class City(Resource):
 	def get(self):
-		connection=sqlite3.connect('data.db')
+		connection=sqlite3.connect(DATABASE)
 		cursor=connection.cursor()
 		query="SELECT CITY2,SUM(PASSENGERSTOCITY2) FROM test GROUP BY CITY2"
 		result=cursor.execute(query)
